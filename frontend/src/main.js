ad5cec65c67a30d0062b4260cd44e2707b028362
dpGGagainis <girts.gagainis@gmail.com>
2025-06-04 09:49:09 +0000

    juu

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,7 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 // Configure axios defaults
-axios.defaults.baseURL = process.env.VUE_APP_API_URL || 'http://46.101.117.113/api'
+axios.defaults.baseURL = 'http://46.101.117.113'
 axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
 
 const app = createApp(App)
