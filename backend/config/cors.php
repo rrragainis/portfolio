b6e264864111c32ce075c1f52560e48f9c8a937f
dpGGagainis <39499184+gloopjuice@users.noreply.github.com>
2025-06-17 17:36:51 +0300

    zxc
    zxc

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -5,7 +5,7 @@ return [
     'allowed_methods' => ['*'],
     'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
-    'allowed_headers' => ['*'],
+    'allowed_headers' => ['X-Requested-With', 'Content-Type', 'Accept', 'Authorization', 'X-CSRF-TOKEN'],
     'exposed_headers' => ['*'],
     'max_age' => 1728000, // 20 days
     'supports_credentials' => true,
