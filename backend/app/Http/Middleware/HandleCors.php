733a4fa77bcbde31da24dd5107c061137fa13392
dpGGagainis <39499184+gloopjuice@users.noreply.github.com>
2025-06-17 17:44:38 +0300

    asd
    asd

--- backend/app/Http/Middleware/HandleCors.php
+++ backend/app/Http/Middleware/HandleCors.php
@@ -4,11 +4,19 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class HandleCors
 {
     public function handle(Request $request, Closure $next)
     {
+        Log::info('CORS Middleware - Request received', [
+            'method' => $request->method(),
+            'path' => $request->path(),
+            'headers' => $request->headers->all(),
+            'origin' => $request->header('Origin')
+        ]);
+
         $response = $next($request);
 
         $response->headers->set('Access-Control-Allow-Origin', '*');
@@ -18,9 +26,14 @@ class HandleCors
         $response->headers->set('Access-Control-Max-Age', '1728000');
 
         if ($request->isMethod('OPTIONS')) {
+            Log::info('CORS Middleware - Handling OPTIONS request');
             $response->setStatusCode(200);
             $response->setContent('');
         }
+
+        Log::info('CORS Middleware - Response headers', [
+            'headers' => $response->headers->all()
+        ]);
 
         return $response;
     }
