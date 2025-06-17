0fb0df34f1008e347215c6662ce8c626b8d48752
dpGGagainis <39499184+gloopjuice@users.noreply.github.com>
2025-06-17 17:31:57 +0300

    zxc
    zxc

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -3,20 +3,10 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => [
-        'http://localhost:8080',
-        'https://*.github.io',
-        env('FRONTEND_URL', 'http://localhost:8080'),
-        'http://46.101.117.113',
-        'https://46.101.117.113',
-        'http://www.rrragainis.lv',
-        'https://www.rrragainis.lv',
-        'http://rrragainis.lv',
-        'https://rrragainis.lv'
-    ],
+    'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
-    'exposed_headers' => [],
+    'exposed_headers' => ['*'],
     'max_age' => 0,
     'supports_credentials' => true,
 ]; 
